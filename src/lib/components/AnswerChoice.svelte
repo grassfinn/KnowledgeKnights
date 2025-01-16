1132a4727de62b038fb84b443d7698ad808e62c4
grassfinn <theharper@live.com>
2025-01-16 08:05:05 -0500

    component setup

---
+++ src/lib/components/AnswerChoice.svelte
@@ -0,0 +1,29 @@
+<script>
+  const { answer, correct } = $props();
+  let bgColor = $state('');
+  console.log(answer);
+
+  function reset() {
+    bgColor = '';
+  }
+
+  function check(e) {
+    if (e.target.textContent === correct) {
+      bgColor = 'green';
+      setTimeout(() => {
+        reset();
+        // questionList.shift();
+      }, 750);
+    } else {
+      bgColor = 'red';
+    }
+  }
+</script>
+
+<li>
+  <button class="answer-choice" style:background-color={bgColor} onclick={check}
+    >{answer}</button
+  >
+</li>
+
+<style></style>
---
+++ src/lib/components/QuizArea.svelte
@@ -0,0 +1,54 @@
+<script>
+  import { questions } from '../../assets/questions.json';
+  import Question from './Question.svelte';
+  let questionList = $state(questions);
+  let currentQuestion = $derived(questionList[0]);
+  const outOfQuestions = $derived(questionList.length > 0);
+  let color = $state('');
+
+</script>
+
+<main>
+  {#if !outOfQuestions}
+    <p>Out of Questions</p>
+  {:else}
+    <Question question={currentQuestion} />
+  {/if}
+  <button onclick={() => questionList.shift()}>Push</button>
+</main>
+
+<style>
+  main {
+    max-width: 1200px;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0 auto;
+  }
+
+  li {
+    text-align: left;
+    list-style: none;
+  }
+
+  .answer-choice {
+    transition: background-color 0.5s;
+  }
+
+  button {
+    width: 300px;
+    min-height: 100px;
+  }
+  ul {
+    margin-top: -0.7em;
+    display: flex;
+    width: 625px;
+    flex-wrap: wrap;
+    gap: 0.5em;
+  }
+</style>
